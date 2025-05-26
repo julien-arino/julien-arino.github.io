330b307a6a7c4f6bfb2854c5e9e8f7b0e0f5d979
julien-arino <51942189+julien-arino@users.noreply.github.com>
2025-05-26 23:18:25 +0000

    Deploying to gh-pages from @ julien-arino/renewed-website@17e2dbf35f11bae3da0da2bf80d9ccb738dc1c1c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -53,7 +53,7 @@ ninja.data = [{
           },
         },{id: "nav-people",
           title: "people",
-          description: "members of the lab or group",
+          description: "members of the group",
           section: "Navigation",
           handler: () => {
             window.location.href = "/renewed-website/people/";
